7cb446cbe686a3b1faff80b2bff1e67cb238dc97
Germán Raúl <german@MacBook-Air-de-German.local>
2023-08-24 12:11:52 -0500

    Update .DS_Store, project.pbxproj, and 39 more files...

---
+++ i_am_rich/I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Germán Raúl on 8/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
